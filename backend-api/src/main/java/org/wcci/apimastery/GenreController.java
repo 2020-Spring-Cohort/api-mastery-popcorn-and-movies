ee13015de8757959fe964052ea418edde08b274c
bwaterman8 <bwaterman8@gmail.com>
2020-03-23 19:26:06 -0400

    made endpoints and get delete post and patch requests for genre controller

--- backend-api/src/main/java/org/wcci/apimastery/GenreController.java
+++ backend-api/src/main/java/org/wcci/apimastery/GenreController.java
@@ -1,22 +1,56 @@
 package org.wcci.apimastery;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+
 @RestController
+@RequestMapping("/genres")
 public class GenreController {
 
     private GenreRepository genreRepository;
+    private MovieRepository movieRepository;
+    private ActorRepository actorRepository;
 
-    public GenreController(GenreRepository genreRepository) {
+
+    public GenreController(GenreRepository genreRepository, MovieRepository movieRepository, ActorRepository actorRepository) {
         this.genreRepository = genreRepository;
+        this.movieRepository = movieRepository;
+        this.actorRepository = actorRepository;
 
     }
-    @RequestMapping("/genres")
+
+    @GetMapping("")
     public Collection<Genre> retrievedGenres() {
         return (Collection<Genre>) genreRepository.findAll();
-
     }
 
+    @GetMapping("/{id}")
+    public Genre displaySingleGenre(@PathVariable long id) {
+        return genreRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGenre(@PathVariable Long id) {
+        Genre genreToRemove = genreRepository.findById(id).get();
+
+        for (Movie movieToRemove : genreToRemove.getMovies()) {
+            for (Actor actorToRemove : movieToRemove.getActors()) {
+                actorRepository.delete(actorToRemove);
+            }
+            movieRepository.delete(movieToRemove);
+        }
+        genreRepository.deleteById(id);
+    }
+    @PostMapping("")
+    public Genre createGenre(@RequestBody Genre genreToAdd) {
+        return genreRepository.save(genreToAdd);
+    }
+    @PatchMapping("/{id}/movies")
+    public Genre updateGenre(@PathVariable Long id, @RequestBody Movie movieToPatch) {
+        Genre genreToUpdate = genreRepository.findById(id).get();
+        Movie movieToAdd = new Movie(movieToPatch.getTitle(), genreToUpdate, (Actor) movieToPatch.getActors());
+        movieRepository.save(movieToAdd);
+        return genreRepository.save(genreToUpdate);
+    }
 }
